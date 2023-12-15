48241ab97981e08fff9d9401533c79ff4ce1e0e2
JamieHeffo <98745575+JamieHeffo@users.noreply.github.com>
2023-12-15 19:57:58 +0000

    Added Laser Guns to player ship

--- player.gd
+++ player.gd
@@ -4,6 +4,12 @@ const MAXSPEED = 30
 const ACCELERATION = 0.75
 var inputVector = Vector3()
 
+@onready var guns = [$Gun1, $Gun2]
+@onready var main = get_tree().current_scene
+
+var Bullet = load("res://bullet.tscn")
+
+
 func _physics_process(delta):
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
@@ -12,6 +18,7 @@ func _physics_process(delta):
 	velocity.x = move_toward(velocity.x, inputVector.x * MAXSPEED, ACCELERATION)
 	velocity.y = move_toward(velocity.y, inputVector.y * MAXSPEED, ACCELERATION)
 	
+	# Set rotation for floaty spacey feel
 	rotation_degrees.z = velocity.x * -2
 	rotation_degrees.x = velocity.y / 2
 	rotation_degrees.y = -velocity.x /2
@@ -21,3 +28,11 @@ func _physics_process(delta):
 	#Restrict player to window
 	transform.origin.x = clamp(transform.origin.x, -15, 15)
 	transform.origin.y = clamp(transform.origin.y, -10, 10)
+	
+	#Pew Pew
+	if Input.is_action_just_pressed("ui_accept"):
+		for i in guns:
+			var bullet = Bullet.instantiate()
+			main.add_child(bullet)
+			bullet.transform = i.global_transform
+			bullet.velocity = bullet.transform.basis.z * -300
