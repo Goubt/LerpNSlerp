2ab4c82715a13ae05a0fb213b49f9a553a774f79
JamieHeffo <98745575+JamieHeffo@users.noreply.github.com>
2023-12-16 14:00:28 +0000

    Added enemy death sound
    Not working properly

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -1,9 +1,29 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
+# Reference the main scene
 @onready var main = get_tree().current_scene
+
+# Reference the enemy scene
 var Enemy = load("res://enemy.tscn")
+
+# Define the grid size for enemy spawning
+var enemyGridSize = Vector2(5, 3)  # Adjust grid size as needed
+
+# Define the spacing between enemies in the grid
+var enemySpacing = Vector3(4, 4, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Spawn enemies in a grid pattern
+	spawn_enemy_grid()
+
+# Spawn enemies in a grid pattern
+func spawn_enemy_grid():
+	for i in range(enemyGridSize.x):
+		var enemy = Enemy.instantiate()
+		main.add_child(enemy)
+		enemy.transform.origin = transform.origin + Vector3(i * enemySpacing.x, randf_range(-10, 10) ,randf_range(3, 3))
 
 func spawn():
 	var enemy = Enemy.instantiate()
@@ -14,6 +34,6 @@ func spawn():
 func _process(delta):
 	pass
 
-
 func _on_timer_timeout():
-	spawn()
+	spawn_enemy_grid()
+
--- enemy.gd
+++ enemy.gd
@@ -1,17 +1,16 @@
 extends CharacterBody3D
 
 var spd = randf_range(20,50)
+var amplitude = 5  # Adjust as needed
+var frequency = 0.2  # Adjust as needed
 
 func _physics_process(delta):
-	
-	# Apply the speed to z axis to move enemy forward
-	velocity.x = 0
-	velocity.y = 0
+	velocity.x = amplitude * sin(frequency * transform.origin.y)
 	velocity.z = spd
 
 	move_and_slide()
 	
 	#remove the enemy when they get behind the player
 	if transform.origin.z > 10:
-		queue_free
+		queue_free()
 	
