557d55a3dd993efd37d959e8b8b75aa348f0b1da
FinnMag <finnmaguire2002@gmail.com>
2023-12-17 20:08:29 +0000

    Added star particles

--- Bullet.gd
+++ Bullet.gd
@@ -8,10 +8,9 @@ func _physics_process(delta):
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Enemies"):
-		
 		player.enemyHit.play()
 			
 		body.queue_free()
 		queue_free()
 		
-		
+	
