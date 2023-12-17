fbf48097d2757b76053c9b200f8bb08a58b5f4e1
FinnMag <finnmaguire2002@gmail.com>
2023-12-17 20:25:33 +0000

    Scoreboard functionality

--- Bullet.gd
+++ Bullet.gd
@@ -9,7 +9,7 @@ func _physics_process(delta):
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Enemies"):
 		player.enemyHit.play()
-			
+		Global.score += 1
 		body.queue_free()
 		queue_free()
 		
