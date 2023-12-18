c25a39ae9384376fb07bf8190e28ed0215656618
JamieHeffo <125657344+JamieHeffernan@users.noreply.github.com>
2023-12-18 16:54:32 +0000

    Update menu.gd
    Menu works, but its ugly

--- menu.gd
+++ menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene("res://main.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_quit_button_pressed():
